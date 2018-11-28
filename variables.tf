739bf406156f6a81ff6a3d4c39e648c1164231b8
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-11-28 05:51:44 -0600

    fixed typo on ami

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "aws_region" {
 variable "aws_amis" {
   default = {
     us-east-1 = "ami-0f9cf087c1f27d9b1"
-    us-esat-2 = "ami-0653e888ec96eab9b"
+    us-east-2 = "ami-0653e888ec96eab9b"
     us-west-1 = "ami-08945b3352c7bf9c0"
     us-west-2 = "ami-076e276d85f524150"
   }
