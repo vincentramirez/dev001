39e07ee44d103dafbc2c4868b2247ff4d99f6dcf
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-12-07 15:29:46 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,9 +21,4 @@ variable "aws_amis" {
   }
 }
 
-variable infoblox_username {}
-variable infoblox_password {}
-variable infoblox_host {}
-variable infoblox_sslverify {}
-variable infoblox_usecookies {}
-variable ad_server_domain {}
+
