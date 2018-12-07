a9a6b6c562f1375aaffb9f006b13c324931237ca
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-12-07 15:29:00 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,3 +20,10 @@ variable "aws_amis" {
     us-west-2 = "ami-076e276d85f524150"
   }
 }
+
+variable infoblox_username {}
+variable infoblox_password {}
+variable infoblox_host {}
+variable infoblox_sslverify {}
+variable infoblox_usecookies {}
+variable ad_server_domain {}
