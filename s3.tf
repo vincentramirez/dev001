86df8607c0dc2a45af901a1db98a82a79d1d1f9f
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-01-09 16:12:50 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "examples3" {
    server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
